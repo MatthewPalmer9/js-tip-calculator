a88555a40727868bfc84016cd2630269ef280180
MacBookPro <macbookpro@MacBookPros-MBP.attlocal.net>
2020-10-26 10:10:23 -0500

    Added minimal CSS styling for the calculator container and set the state handling for the bill total.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import React from 'react';
+import Calc from './components/calculator.jsx';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      Hello from App
+      <Calc />
     </div>
   );
 }
--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,10 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../styles/calculator.css';
 
 export default class calculator extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            billTotal: 0,
+            servicePerc: 0,
+            totalCustomers: 0
+        }
+    }
+
+    onTotalChange = (event) => {
+        this.setState({
+            billTotal: event.target.value
+        });
+    }
+
     render() {
         return (
-            <div>
-                
+            <div className="container">
+                <div className="calculator">
+
+                    <div className="bill-input">
+                        Bill Total: <input onChange={this.onTotalChange}></input>
+                        {this.state.billTotal}
+                    </div>
+
+                </div>
             </div>
         )
     }
